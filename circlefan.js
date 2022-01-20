f5944141956d3d1370ed622f037e51b0e5644130
Steinar Logi <steinarlg@gmail.com>
2022-01-20 11:35:34 +0000

    Vefsíða

---
+++ circlefan.js
@@ -0,0 +1,78 @@
+/////////////////////////////////////////////////////////////////
+//    S�nid�mi � T�lvugraf�k
+//     Teikna n�lgun � hring sem TRIANGLE_FAN
+//
+//    Hj�lmt�r Hafsteinsson, jan�ar 2022
+/////////////////////////////////////////////////////////////////
+var canvas;
+var gl;
+
+// numCirclePoints er fj�ldi punkta � hringnum
+// Heildarfj�ldi punkta er tveimur meiri (mi�punktur + fyrsti punktur kemur tvisvar)
+var numCirclePoints = 20;
+
+var radius = 0.4;
+var center = vec2(0, 0);
+
+var points = [];
+
+window.onload = function init() {
+
+    canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+	// Create the circle
+    //points.push( center );
+    createCirclePoints( center, radius, numCirclePoints );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    render();
+}
+
+
+// Create the points of the circle
+function createCirclePoints( cent, rad, k )
+{
+    var dAngle = 2*Math.PI/k;
+
+    for( i=k; i>=0; i-- ) {
+        //Þarf fyrst að bæta inn miðjupunktinum í hverri ítrun.
+        points.push(cent)
+        //bæti við punkti á hringnum.
+        a = i*dAngle;
+        var p = vec2( rad*Math.sin(a) + cent[0], rad*Math.cos(a) + cent[1] );
+        points.push(p);
+        //Bæti við næsta punkti á hringnum.
+        a1 = (i+1)*dAngle;
+        var p2 = vec2(rad*Math.sin(a1) + cent[0], rad*Math.cos(a1) + cent[1] );
+        points.push(p2)
+    }
+}
+
+function render() {
+
+    gl.clear( gl.COLOR_BUFFER_BIT );
+
+    // Draw circle using Triangles
+    gl.drawArrays( gl.TRIANGLES, 0, numCirclePoints*3);
+
+    window.requestAnimFrame(render);
+}
